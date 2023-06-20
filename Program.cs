da67cf6b84eab6bdaaead3428609ffa464a17826
Noah Pilarski <mehmehmeh313@gmail.com>
2023-06-20 18:53:14 -0400

    Change back

--- Program.cs
+++ Program.cs
@@ -1,83 +1,86 @@
 ﻿using System.Globalization;
 using System.Text.Json;
 
-class Core 
+class Core
 {
-	static void Main(string[] args)
-	{
-		var logFile = File.ReadAllText(args[0]);
+    static void Main(string[] args)
+    {
+        var logFile = File.ReadAllText(args[0]);
 
-		var splits = logFile.Split($"\r\ncommit ");
+        var splits = logFile.Split($"\r\ncommit ");
 
-		List<Commit> commits = new();
-		foreach (var split in splits)
-		{
-			var commit = new Commit(split.Split("\r\n"));
-			commits.Add(commit);
-		}
+        List<Commit> commits = new();
+        foreach (var split in splits)
+        {
+            var commit = new Commit(split.Split("\r\n"));
+            commits.Add(commit);
+        }
 
-		var updates = commits.SelectMany(x => x.Updates);
-		var includedMods = updates.Select(x => x.Repo).Distinct();
+        var updates = commits.SelectMany(x => x.Updates);
+        var includedMods = updates.Select(x => x.Repo).Distinct();
 
-		List<Entry> entries = new();
-		foreach (var mod in includedMods)
-		{
-			List<DownloadCountUpdate> countUpdates = new();
-			foreach (var update in updates)
-			{
-				if (update.Repo == mod)
-				{
-					countUpdates.Add(new DownloadCountUpdate(((DateTimeOffset)update.Time).ToUnixTimeSeconds(), update.DownloadCount));
-				}
-			}
+        List<Entry> entries = new();
+        foreach (var mod in includedMods)
+        {
+            List<DownloadCountUpdate> countUpdates = new();
+            foreach (var update in updates)
+            {
+                if (update.Repo == mod)
+                {
+                    countUpdates.Add(new DownloadCountUpdate(((DateTimeOffset)update.Time).ToUnixTimeSeconds(), update.DownloadCount));
+                }
+            }
 
-			if (Offsets.offsetBetween.ContainsKey(mod))
-			{
-				Offsets.Between offsetBetween = Offsets.offsetBetween[mod];
-				foreach (DownloadCountUpdate update in countUpdates)
-				{
-					long unixTimestamp = update.UnixTimestamp;
-					if (unixTimestamp > offsetBetween.AfterUnixTimestamp && unixTimestamp <= offsetBetween.BeforeUnixTimestamp)
-					{
-						update.DownloadCount += offsetBetween.DownloadCount;
-					}
-				}
-			}
+            if (Offsets.offsetBetween.ContainsKey(mod))
+            {
+                List<Offsets.Between> offsetBetween = Offsets.offsetBetween[mod];
+                foreach (DownloadCountUpdate update in countUpdates)
+                {
+                    long unixTimestamp = update.UnixTimestamp;
+                    foreach (var between in offsetBetween)
+                    {
+                        if (unixTimestamp > between.AfterUnixTimestamp && unixTimestamp <= between.BeforeUnixTimestamp)
+                        {
+                            update.DownloadCount += between.DownloadCount;
+                        }
+                    }
+                }
+            }
 
-			if (Offsets.offsets.ContainsKey(mod))
-			{
-				countUpdates.AddRange(Offsets.offsets[mod]);
-			}
+            if (Offsets.offsets.ContainsKey(mod))
+            {
+                countUpdates.AddRange(Offsets.offsets[mod]);
+            }
 
-			var entry = new Entry()
-			{
-				Repo = mod,
-				Updates = countUpdates.ToArray()
-			};
+            var entry = new Entry()
+            {
+                Repo = mod,
+                Updates = countUpdates.ToArray()
+            };
 
-			entries.Add(entry);
-		}
+            entries.Add(entry);
+        }
 
-		var json = JsonSerializer.Serialize(entries);
+        var json = JsonSerializer.Serialize(entries);
 
-		Console.WriteLine(json);
-	}
+        Console.WriteLine(json);
+    }
 
-	public static int GetNthIndex(string s, char t, int n)
-	{
-		var count = 0;
-		for (var i = 0; i < s.Length; i++)
-		{
-			if (s[i] == t)
-			{
-				count++;
-				if (count == n)
-				{
-					return i;
-				}
-			}
-		}
+    public static int GetNthIndex(string s, char t, int n)
+    {
+        var count = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == t)
+            {
+                count++;
+                if (count == n)
+                {
+                    return i;
+                }
+            }
+        }
 
-		return -1;
-	}
+        return -1;
+    }
 }
